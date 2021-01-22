3f7f1f40951b4ae561e1fd0bd6a9a579e5fedba1
Joshua Galit <joshuaimalay@gmail.com>
2021-01-22 09:28:38 +0800

    import TheNavBar component and wrap Nuxt in main tag

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,5 +1,16 @@
 <template>
   <div class="bg-gray-100 text-gray-900 leading-tight">
-    <Nuxt />
+    <TheNavBar />
+    <main>
+      <Nuxt />
+    </main>
   </div>
 </template>
+
+<script>
+  export default {
+    components: {
+      TheNavBar: () => import('~/components/TheNavBar')
+    }
+  }
+</script>
