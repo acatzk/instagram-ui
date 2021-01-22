f4bbbf93ad49fdc72bf253c5a896e4e1af025d20
Joshua Galit <joshuaimalay@gmail.com>
2021-01-22 21:50:08 +0800

    extend color funchsia

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,5 @@
+const colors = require('tailwindcss/colors')
+
 module.exports = {
   purge: [
     './components/**/*.{vue,js}',
@@ -8,7 +10,11 @@ module.exports = {
   ],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        fuchsia: colors.fuchsia
+      }
+    },
   },
   variants: {
     extend: {},
