95ff0d399667ab75435a347e25b4bac8f39dcd24
Joshua Galit <joshuaimalay@gmail.com>
2021-01-22 22:20:48 +0800

    add relative top-1 in logo adjust link icon spacing to left-right of 6

--- components/TheNavBar.vue
+++ components/TheNavBar.vue
@@ -2,7 +2,7 @@
   <header class="sticky top-0 bg-white border-b border-gray-300">
     <div class="container mx-auto max-w-5xl py-3 px-5">
       <div class="flex items-center justify-between">
-        <div class="flex-shrink-0">
+        <div class="flex-shrink-0 relative top-1">
           <nuxt-link to="/">
             <img src="~/assets/images/instagram.png" alt="logo">
           </nuxt-link>
@@ -16,7 +16,7 @@
                   class="text-sm font-light text-center py-1 bg-gray-50 rounded-sm  w-52 border border-gray-300 focus:ring-0" 
                   type="text" />
         </div>
-        <div class="flex items-center space-x-5">
+        <div class="flex items-center space-x-6">
           <a href="#" class="block" @click="isHome = !isHome">
             <svg class="w-5 h-5" viewBox="0 0 48 48">
               <path v-if="isHome" d="M45.3 48H30c-.8 0-1.5-.7-1.5-1.5V34.2c0-2.6-2-4.6-4.6-4.6s-4.6 2-4.6 4.6v12.3c0 .8-.7 1.5-1.5 1.5H2.5c-.8 0-1.5-.7-1.5-1.5V23c0-.4.2-.8.4-1.1L22.9.4c.6-.6 1.5-.6 2.1 0l21.5 21.5c.4.4.6 1.1.3 1.6 0 .1-.1.1-.1.2v22.8c.1.8-.6 1.5-1.4 1.5zm-13.8-3h12.3V23.4L24 3.6l-20 20V45h12.3V34.2c0-4.3 3.3-7.6 7.6-7.6s7.6 3.3 7.6 7.6V45z"></path>
