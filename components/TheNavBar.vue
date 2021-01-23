63050c88b1dd1cadf5e7669a02e64c42b07ff5ba
Joshua Galit <joshuaimalay@gmail.com>
2021-01-23 22:49:39 +0800

    change border-b color to border-gray-200

--- components/TheNavBar.vue
+++ components/TheNavBar.vue
@@ -1,5 +1,5 @@
 <template>
-  <header class="sticky top-0 bg-white border-b border-gray-300">
+  <header class="sticky top-0 bg-white border-b border-gray-200">
     <div class="container mx-auto max-w-5xl py-3 px-5">
       <div class="flex items-center justify-between">
         <div class="flex-shrink-0 relative top-1">
